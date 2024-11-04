75402ebc4febb9719160d962a88d11e8f038b9c3
聂元昊 <200834590@qq.com>
2024-11-04 09:42:21 +0800

    feat: add function `DoGLM4` for glm-4 async result.

--- async_result.go
+++ async_result.go
@@ -28,6 +28,16 @@ type AsyncResultResponse struct {
 	VideoResult []AsyncResultVideo `json:"video_result"`
 }
 
+// AsyncResultGLM4Response is the GLM4 response of the AsyncResultService
+type AsyncResultGLM4Response struct {
+	Model      string                 `json:"model"`
+	Choices    []ChatCompletionChoice `json:"choices"`
+	TaskStatus string                 `json:"task_status"`
+	RequestID  string                 `json:"request_id"`
+	ID         string                 `json:"id"`
+	Usage      ChatCompletionUsage    `json:"usage"`
+}
+
 // NewAsyncResultService creates a new async result get service
 func NewAsyncResultService(client *Client) *AsyncResultService {
 	return &AsyncResultService{
@@ -42,6 +52,27 @@ func (s *AsyncResultService) SetID(id string) *AsyncResultService {
 }
 
 func (s *AsyncResultService) Do(ctx context.Context) (res AsyncResultResponse, err error) {
+	var (
+		resp     *resty.Response
+		apiError APIErrorResponse
+	)
+
+	if resp, err = s.client.request(ctx).
+		SetResult(&res).
+		SetError(&apiError).
+		Get("async-result/" + s.id); err != nil {
+		return
+	}
+
+	if resp.IsError() {
+		err = apiError
+		return
+	}
+
+	return
+}
+
+func (s *AsyncResultService) DoGLM4(ctx context.Context) (res AsyncResultGLM4Response, err error) {
 	var (
 		resp     *resty.Response
 		apiError APIErrorResponse
